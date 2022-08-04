8a618f1758a3654bc605f12e1de706c525fd3e61
mcantop <maciejcantop@icloud.com>
2022-08-04 21:40:38 +0200

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Maciej on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
